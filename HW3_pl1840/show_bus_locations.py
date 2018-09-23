f75415f67c93b02951404dcedbe669c3df0bbe8c
Pengzi Li <41458354+estellaleee@users.noreply.github.com>
2018-09-23 19:27:06 -0400

    Update show_bus_locations.py

--- HW3_pl1840/show_bus_locations.py
+++ HW3_pl1840/show_bus_locations.py
@@ -7,17 +7,14 @@ try:
 except ImportError:
     import urllib.request as urllib
 
-pl.rc('font', size=15)
 
 key = sys.argv[1]
 line = sys.argv[2]
 
-#defining URL to obtain the information from MTA API
+
 url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=" + key + "&LineRef=" + line
 response = urllib.urlopen(url)
-#reading the response from the site and decoding it to UTF-8 format
 data = response.read().decode("utf-8")
-#converting the file to json format
 data = json.loads(data)
 
 bus=len(data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'])
